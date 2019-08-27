3fbae04d3501a97f6a757a6353a4cff3d97f4d3b
Rob Rudin <rob.rudin@marklogic.com>
2019-08-27 11:54:09 -0400

    Fixed missing import

--- src/main/java/com/marklogic/grove/boot/auth/AuthController.java
+++ src/main/java/com/marklogic/grove/boot/auth/AuthController.java
@@ -8,6 +8,7 @@ import com.marklogic.client.io.InputStreamHandle;
 import com.marklogic.grove.boot.AbstractController;
 import com.marklogic.grove.boot.MarkLogicConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
